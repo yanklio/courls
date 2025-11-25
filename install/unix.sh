bc3306ed5423e478ad1dba9b9c09ab1f8c694501
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2025-11-25 11:33:41 +0100

    feat: add auto-install script for command in unix machines

--- install/unix.sh
+++ install/unix.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-#
-# steps to install courls for unix systems
-chmod +x courls
-sudo mv courls /usr/local/bin/courls
+
+REPO="yanklio/courls"
+BINARY="courls"
+DEST="/usr/local/bin"
+
+# Get the latest tag
+TAG=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep -o '"tag_name": ".*"' | sed 's/"tag_name": "//;s/"//')
+
+# Direct link to the raw binary (not the tar.gz)
+URL="https://github.com/$REPO/releases/download/$TAG/$BINARY"
+
+echo "Downloading $BINARY version $TAG..."
+curl -L $URL -o /tmp/$BINARY
+
+# Install
+chmod +x /tmp/$BINARY
+echo "Installing $BINARY to $DEST..."
+sudo mv /tmp/$BINARY $DEST/$BINARY
+
+echo "Done!"
